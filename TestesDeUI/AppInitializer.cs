3a5f3701fe5d3f1aca142b551d5859e11316bf04
Ricardo Prestes <ricardo.logan@hotmail.com>
2021-07-21 08:58:58 -0300

    adicionando path do apk

--- TestesDeUI/AppInitializer.cs
+++ TestesDeUI/AppInitializer.cs
@@ -1,42 +1,24 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
 using Xamarin.UITest;
-using Xamarin.UITest.Queries;
 
 namespace TestesDeUI
 {
     public class AppInitializer
     {
         public static IApp StartApp(Platform platform)
-        {
-            // TODO: If the iOS or Android app being tested is included in the solution 
-            // then open the Unit Tests window, right click Test Apps, select Add App Project
-            // and select the app projects that should be tested.
-            //
-            // The iOS project should have the Xamarin.TestCloud.Agent NuGet package
-            // installed. To start the Test Cloud Agent the following code should be
-            // added to the FinishedLaunching method of the AppDelegate:
-            //
-            //    #if ENABLE_TEST_CLOUD
-            //    Xamarin.Calabash.Start();
-            //    #endif
-
-            
+        {            
+            string appBundlePath = Environment.GetEnvironmentVariable("APP_BUNDLE_PATH");
 
             if (platform == Platform.Android)
             {
                 return ConfigureApp
                     .Android
-                    // TODO: Update this path to point to your Android app and uncomment the
-                    // code if the app is not included in the solution.
-                    //.ApkFile ("../../../Droid/bin/Debug/xamarinforms.apk")
+                    .ApkFile (appBundlePath)
                     .StartApp();
             }
 
             var iOSConfiguration = ConfigureApp.iOS;
 
-            string appBundlePath = Environment.GetEnvironmentVariable("APP_BUNDLE_PATH");
             if (!string.IsNullOrEmpty(appBundlePath))
             {
                 iOSConfiguration.AppBundle(appBundlePath);
